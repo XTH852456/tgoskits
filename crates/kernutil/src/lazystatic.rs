dc959e2f12e917f6c03e3fe3942fef2e39906ef6
周睿 <zrufo747@outlook.com>
2025-11-30 23:45:01 +0800

    feat: 添加更新已初始化值的功能，增强LazyStatic的使用灵活性

--- crates/kernutil/src/lazystatic.rs
+++ crates/kernutil/src/lazystatic.rs
@@ -31,6 +31,18 @@ impl<T> LazyStatic<T> {
         unsafe { (*self.value.get()).as_mut_ptr().write(val) };
         self.init.store(true, Ordering::Release);
     }
+
+    /// 更新已初始化的值
+    /// # Safety
+    /// 调用者必须确保该值已经初始化
+    /// thread-unsafe
+    pub unsafe fn update<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+        if !self.init.load(Ordering::Acquire) {
+            panic!("LazyStatic {} not initialized", core::any::type_name::<T>());
+        }
+        let val = unsafe { &mut *(*self.value.get()).as_mut_ptr() };
+        f(val)
+    }
 }
 
 impl<T> Deref for LazyStatic<T> {
