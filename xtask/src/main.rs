9fb92f222b285d062081bfbd206c376a4511895c
周睿 <zrufo747@outlook.com>
2026-01-21 22:10:46 +0800

    ✨ feat(xtask): 更新主函数配置以支持无操作系统环境

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,6 @@
-#![cfg(any(windows, unix))]
 #![cfg_attr(target_os = "none", no_main)]
+#![cfg_attr(target_os = "none", no_std)]
+#![cfg(not(target_os = "none"))]
 
 fn main() {
     println!("Hello, world!");
